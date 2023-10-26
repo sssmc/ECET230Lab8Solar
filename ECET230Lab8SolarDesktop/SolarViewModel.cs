60ca4cfa20eacc04ec5be7fea3bd66eff6b361a9
Sebastien Robitaille <sebastien@robitaille.info>
2023-10-25 22:17:30 -0700

    started serial implimentation in view model

--- ECET230Lab8SolarDesktop/SolarViewModel.cs
+++ ECET230Lab8SolarDesktop/SolarViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
+using System.IO.Ports;
+using System.Text;
+using System.Windows.Input;
+
 namespace ECET230Lab8SolarDesktop;
 
 public class SolarViewModel : INotifyPropertyChanged
@@ -9,7 +13,60 @@ public class SolarViewModel : INotifyPropertyChanged
 
     private string _rawPacket;
 
-    public SolarViewModel() { }
+    private SerialPort _serialPort;
+
+    private bool _comPortOpen;
+
+    public ICommand ChangeComPortCommand { get; private set;  }
+
+    public ICommand StartStopComPortCommand { get; private set;}
+
+    public bool ComPortOpen
+    {
+        get => _comPortOpen;
+        set
+        {
+            _comPortOpen = value;
+            OnPropertyChanged();
+            if (_comPortOpen)
+                _serialPort.Open();
+            else
+                _serialPort.Close();
+        }
+    }
+
+    public string[] ComPorts
+    {
+        get => SerialPort.GetPortNames();
+
+    }
+
+    public SolarViewModel() {
+        _serialPort = new();
+        _serialPort.DataReceived += SerialPort_DataReceived;
+
+        this.ComPortOpen = false;
+
+        ChangeComPortCommand = new Command<string>(ChangeComPort);
+
+        StartStopComPortCommand = new Command(StartStopComPort);
+
+    }
+
+    private void StartStopComPort()
+    {
+        this.ComPortOpen = !this.ComPortOpen;
+    }
+
+    private void ChangeComPort(string comPort)
+    {
+        _serialPort.PortName = comPort;
+    }
+
+    private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+    {
+        this.RawPacket = _serialPort.ReadLine();
+    }
 
     public string RawPacket
     {
